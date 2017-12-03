52203ae3f9fe366520eca00b865d5b71c9903440
LeYang <yangle_nwpy@mail.nwpu.edu.cn>
2017-12-03 10:55:11 +0800

    delete

--- PrepareData/Caffe_style/compImgMean.sh
+++
@@ -1,7 +0,0 @@
-echo "Compute mean file of the image set..."
-GLOG_logtostderr=0 GLOG_log_dir=./Log/ \
-/home/yangle/software/caffe/.build_release/tools/compute_image_mean \
-/home/yangle/TCyb/dataset/patch_MSRA/lmdb/train_img_lmdb \
-/home/yangle/TCyb/dataset/patch_MSRA/lmdb/imageMean.binaryproto 
-
-echo "Done."
--- PrepareData/Caffe_style/convert_img_lmdb.sh
+++
@@ -1,8 +0,0 @@
-echo "Creating train rgb lmdb..."
-
-/home/yangle/software/caffe/.build_release/tools/convert_imageset \
-/home/yangle/TCyb/dataset/patch_MSRA/image/ \
-/home/yangle/TCyb/dataset/patch_MSRA/name.txt \
-/home/yangle/TCyb/dataset/patch_MSRA/lmdb/train_img_lmdb \
-
-echo "Done."
