93796a13db8d5aab4db408e85ef7fdfebc585327
LeYang <nwpuyangle@gmail.com>
2019-05-22 17:28:20 +0800

    remove materials

--- Materials/helpers/Mount_To_Disk/mount_msralab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#sh ./mount.sh hopeng //172.23.232.98/VC_VIDEO_RECOGNITION$/ ~/msralab_VID
-#sh ./mount.sh hopeng //172.23.232.81/VC_IMAGE_RECOGNITION$/ ~/msralab_IMG
-#sh ./mount.sh v-had //172.23.233.141/msravcg-wp$ ~/msralab_WP
-sh ./mount.sh v-had //10.190.145.25/Share/Groups/MSM/ ~/Tmsrasia_MSM
-#sh ./mount.sh hopeng //10.190.145.25/Users/hopeng/private ~/msrasia_hopeng
-#sh ./mount.sh hopeng //172.23.232.111/MSRAGPUM07$ ~/msralab_gpu07
-
